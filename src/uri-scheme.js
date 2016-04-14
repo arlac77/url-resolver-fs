8ef61f5e40f80673236425ddbd04411d84c3f314
Markus Felten <Markus.Felten@gmx.de>
2016-04-14 18:27:19 +0200

    fix: pass list(),fetch() and history() to baseScheme

--- src/uri-scheme.js
+++ src/uri-scheme.js
@@ -35,19 +35,33 @@ class URIScheme {
 
 class URIMapperScheme extends URIScheme {
 
-  constructor(baseScheme,options) {
+  constructor(baseScheme, options) {
     super();
 
     Object.defineProperty(this, 'baseScheme', {
       value: baseScheme
     });
-    
+
     Object.defineProperty(this, 'name', {
       value: options.name
     });
   }
 
-  remapURI(uri) { return uri; }
+  list(uri, options) {
+    return this.baseScheme.list(uri, options);
+  }
+
+  fetch(uri, options) {
+    return this.baseScheme.fetch(uri, options);
+  }
+
+  history(uri, options) {
+    return this.baseScheme.history(uri, options);
+  }
+
+  remapURI(uri) {
+    return uri;
+  }
 }
 
 exports.URIScheme = URIScheme;
