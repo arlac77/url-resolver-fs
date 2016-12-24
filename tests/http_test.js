a8a8ec6ce305684a02b7fbc9c0bb72385dab68ed
Markus Felten <Markus.Felten@gmx.de>
2016-12-24 14:56:24 +0100

    test: http get & stat

---
+++ tests/http_test.js
@@ -0,0 +1,35 @@
+/* global describe, it, xit, before, after */
+/* jslint node: true, esnext: true */
+'use strict';
+
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should(),
+  HTTPScheme = require('../dist/index').HTTPScheme;
+
+describe('http', () => {
+  const h = new HTTPScheme();
+
+  it('can get', done => {
+    h.get('http://www.heise.de/index.html').then(s => {
+      assert.isDefined(s);
+
+      s.on('data', chunk => {
+        //console.log(chunk.toString('ascii'));
+
+        if (chunk.includes('DOCTYPE')) {
+          done();
+        }
+      });
+    });
+  });
+
+  it('can stat', () => {
+    return h.stat('http://www.heise.de/index.html').then(s => {
+      //console.log(s);
+      assert.equal(s.status, 200);
+      assert.isDefined(s);
+    });
+  });
+});
